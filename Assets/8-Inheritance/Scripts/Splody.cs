22bd4f8b7417f48b4de1ac10b30e95fa22a0b37e
AMeghnad <anshumanmeghnad@gmail.com>
2017-10-10 16:08:26 +1100

    Inheritance deletion and new version upload

---
+++ Assets/8-Inheritance/Scripts/Splody.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inheritance
+{
+    public class Splody : Enemy
+    {
+        [Header("Splodey")]
+        public float splosionRadius = 5f;
+        public float splosionRate = 3f;
+        public float impactForce = 10f;
+        public GameObject splosionParticles;
+
+        private float splosionTimer = 0f;
+
+        public override void Attack()
+        {
+            // Start ignitionTimer
+            // IF splosionTimer > splosionRate
+                // Call Splode()
+        }
+
+        void Splode()
+        {
+            // Perform Physics OverlapSphere with splosionRadius
+                // Loop through all hits
+                    // If player
+                        // Add impactForce to rigidbody
+            // Destroy self
+
+        }
+    }
+}
---
+++ Assets/~AHundredBalls/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hundredballs
+{
+    public class Paddle : MonoBehaviour
+    {
+        public bool isOpened;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isOpened = !isOpened;
+            }
+        }
+    }
+}
